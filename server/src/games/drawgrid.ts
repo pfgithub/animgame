502a53f4824f676d34f463fd1f252f002fb4ce96
pfg <pfg@pfg.pw>
2024-06-20 12:07:26 -0500

    DrawGrid play order

---
+++ server/src/games/drawgrid.ts
@@ -0,0 +1,29 @@
+// play order:
+// - creator sets custom word list if wanted
+// - players join and choose a color palette
+// - once everyone had readied up, draw time
+// - choose your word from a choice of three
+//   - no players will get the same words
+// - once everyone has chosen, draw your
+//   word
+// - once everyone has drawn, reveal
+//   all the drawings in a grid, their positions
+//   are randomized per-player to avoid bias.
+// - guess them all as fast as you can.
+//   - four minute timer? or have it so when
+//     you give up you can press a 'give up'
+//     button and once everyone's done that
+//     move on
+// - keep score. two scores? drawing & guessing?
+//   or just one, combined draw/guess
+// - show end screen with scores
+
+// this doesn't require much new stuff
+// - same palette&ready screen
+// - new ChooseFromList screen
+// - same draw screen with prompt, but
+//   only ask for one frame (and hide the frame
+//   bar if only one frame)
+// - new DrawGridReveal screen
+// - new EndScore screen
+
